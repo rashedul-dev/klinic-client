8d4b83622fb1ded270226e53f524a1b9ca4338d4
rashedul-dev <rashedulislam.edge@gmail.com>
2025-11-25 23:45:36 +0600

    add(auth): client now can persist and store the access and refresh token on cookies

--- src/services/auth/loginUser.ts
+++ src/services/auth/loginUser.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { parse } from "cookie";
+import { cookies } from "next/headers";
 import z from "zod";
+import { fi } from "zod/locales";
 
 const loginValidationZodSchema = z.object({
   email: z.string().min(1, { message: "Email is required" }).email({ message: "Invalid email address" }),
@@ -12,6 +15,9 @@ const loginValidationZodSchema = z.object({
 });
 
 export const loginUser = async (_currentState: any, formData: FormData): Promise<any> => {
+  let accessTokenObject: null | any = null;
+  let refreshTokenObject: null | any = null;
+
   try {
     const loginData = {
       email: formData.get("email"),
@@ -39,8 +45,56 @@ export const loginUser = async (_currentState: any, formData: FormData): Promise
         "Content-Type": "application/json", // Send as JSON
       },
       body: JSON.stringify(loginData), // Stringify the object
-    }).then((res) => res.json());
-    return res;
+    });
+
+    // JSON response body - contain user data and tokens
+    const result = await res.json();
+
+    const setCookieHeaders = res.headers.getSetCookie();
+    if (setCookieHeaders && setCookieHeaders.length > 0) {
+      setCookieHeaders.forEach((cookie: string) => {
+        const parsedCookie = parse(cookie);
+
+        if (parsedCookie["accessToken"]) {
+          accessTokenObject = parsedCookie;
+        }
+        if (parsedCookie["refreshToken"]) {
+          refreshTokenObject = parsedCookie;
+        }
+      });
+    } else {
+      // No Cookie on Headres
+      throw new Error("No set-cookie header found");
+    }
+
+    // Check both token exist
+    if (!accessTokenObject && !refreshTokenObject) {
+      throw new Error("Token not Found");
+    }
+
+    const cookieStore = await cookies();
+
+    cookieStore.set("accessToken", accessTokenObject.accessToken, {
+      httpOnly: true,
+      maxAge: parseInt(accessTokenObject.maxAge) || 1000 * 60 * 60,
+      path: accessTokenObject.path || "/",
+      secure: true,
+      sameSite: accessTokenObject.sameSite || "none",
+    });
+
+    // Set access token on the server side cookie store
+    cookieStore.set("refreshToken", refreshTokenObject.refreshToken, {
+      httpOnly: true,
+      maxAge: parseInt(refreshTokenObject.maxAge) || 1000 * 60 * 60 * 24 * 90,
+      path: refreshTokenObject.path || "/",
+      secure: true,
+      sameSite: refreshTokenObject.sameSite || "none",
+    });
+
+    console.log({ res, result });
+
+    // return the JSON response so that req can handle redirection or other logic
+    return result;
   } catch (error) {
     console.log("Login Error", error);
     return {
